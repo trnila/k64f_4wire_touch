56ad71f5c881d2e804e73226907809b7dcc17ba2
Daniel Trnka <daniel.trnka@gmail.com>
2018-01-28 13:24:40 +0100

    Reformat

--- main.cpp
+++ main.cpp
@@ -11,18 +11,18 @@ Touch touch(PIN_XP, PIN_XM, PIN_YP, PIN_YM);
 Panel panel(touch, 180, 230);
 
 int main() {
-	pc.baud(115200);
+    pc.baud(115200);
     pc.printf("Hello World!\r\n");
 
-	panel.setPressureThreshold(100000);
-	panel.calibrateX(12200, 40700, true);
-	panel.calibrateY(8600, 48900, true);
+    panel.setPressureThreshold(100000);
+    panel.calibrateX(12200, 40700, true);
+    panel.calibrateY(8600, 48900, true);
 
-	int X, Y, RX, RY, pressure;
-	bool pressed;
+    int X, Y, RX, RY, pressure;
+    bool pressed;
     while (true) {
-		pressed = panel.getPosRaw(X, Y, RX, RY, pressure);
-		pc.printf("X=%d Y=%d RX=%d RY=%d pressure=%d pressed=%d\r\n", X, Y, RX, RY, pressure, pressed);
-		wait(0.05f);
+        pressed = panel.getPosRaw(X, Y, RX, RY, pressure);
+        pc.printf("X=%d Y=%d RX=%d RY=%d pressure=%d pressed=%d\r\n", X, Y, RX, RY, pressure, pressed);
+        wait(0.05f);
     }
 }

d378ec47ed212378433f0c0ad18264dcf89a4330
Daniel Trnka <daniel.trnka@gmail.com>
2018-01-29 20:24:45 +0100

    Simplified computing USX/USY

--- main.cpp
+++ main.cpp
@@ -6,32 +6,32 @@
 #define INPUT_METHOD_TOUCH 0
 #define INPUT_METHOD_ACCELEROMETER 1
 
+// configurable
 #define INPUT_METHOD INPUT_METHOD_ACCELEROMETER
+
+
+#define us2dc(t, us) ((double) (us) / ((t)*1000))
 
 const PinName PIN_YM = A0;
 const PinName PIN_XM = A1;
 const PinName PIN_YP = A2;
 const PinName PIN_XP = A3;
 
-Serial pc(USBTX, USBRX);
-Touch touch(PIN_XP, PIN_XM, PIN_YP, PIN_YM);
-TouchPanel panel(touch);
-
-PwmOut servoX(PTC10), servoY(PTC11);
-
 const int MX = 90, MY = 90;
 const int PX = 30, PY = 30;
 const int DUTY_MS = 20;
 const int CENTER_X_US = 1600;
 const int CENTER_Y_US = 1500;
-const double SHIFT_MIN = -0.012; //- 250
-const double SHIFT_MAX = 0.012; // 250
+const double SHIFT_MIN_US = -250;
+const double SHIFT_MAX_US = 250;
 
-#define us2dc(t, us) ((double) (us) / ((t)*1000))
-
+Serial pc(USBTX, USBRX);
+Touch touch(PIN_XP, PIN_XM, PIN_YP, PIN_YM);
+TouchPanel panel(touch);
+PwmOut servoX(PTC10), servoY(PTC11);
 
 double cap(double val) {
-	return min(max(val, SHIFT_MIN), SHIFT_MAX);
+	return std::min(std::max(val, SHIFT_MIN_US), SHIFT_MAX_US);
 }
 
 
@@ -73,8 +73,8 @@ int main() {
 		double DX = angleX * 1800 / M_PI;
 		double DY = angleY * 1800 / M_PI;
 
-		double USX = us2dc(DUTY_MS, CENTER_X_US) + cap(us2dc(DUTY_MS, DX));
-		double USY = us2dc(DUTY_MS, CENTER_Y_US) + cap(us2dc(DUTY_MS, DY));
+		double USX = us2dc(DUTY_MS, CENTER_X_US + cap(DX));
+		double USY = us2dc(DUTY_MS, CENTER_Y_US + cap(DY));
 
 		printf("FXOS8700Q ACC: X=%1.4f Y=%1.4f Z=%1.4f DX=%f DY=%f\r\n", x, y, z, USX, USY);
 		servoX.write(USX);

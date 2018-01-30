c6c733e9171c1c6deb7a881e470157da68e229bc
Daniel Trnka <daniel.trnka@gmail.com>
2018-01-30 10:18:55 +0100

    Upgraded to mbed-os

--- config.h
+++ config.h
@@ -3,6 +3,8 @@
 #define INPUT_METHOD_TOUCH 0
 #define INPUT_METHOD_ACCELEROMETER 1
 
+//#define FUNCTION_CENTER
+
 // configurable
 #define INPUT_METHOD INPUT_METHOD_TOUCH
 
@@ -15,6 +17,8 @@ const int MX = 90, MY = 90;
 const int PX = 30, PY = 30;
 const int DUTY_MS = 20;
 const int CENTER_X_US = 1600;
-const int CENTER_Y_US = 1500;
+const int CENTER_Y_US = 1550;
 const double SHIFT_MIN_US = -250;
 const double SHIFT_MAX_US = 250;
+
+const int MEASUREMENT_PERIOD_MS = DUTY_MS;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <mbed.h>
+#include <mbed_events.h>
 #include "Accelerometer.h"
 #include "Touch.h"
 #include "config.h"
@@ -10,8 +11,43 @@ Touch touch(PIN_XP, PIN_XM, PIN_YP, PIN_YM);
 TouchPanel panel(touch);
 PwmOut servoX(PTC10), servoY(PTC11);
 
+#if INPUT_METHOD == INPUT_METHOD_TOUCH
+	TouchPanel &input = panel;
+#elif INPUT_METHOD == INPUT_METHOD_ACCELEROMETER
+	AccelerometerInput input;
+#else
+	#error "wrong input method"
+#endif
+
+
 double cap(double val) {
 	return std::min(std::max(val, SHIFT_MIN_US), SHIFT_MAX_US);
+}
+
+void control() {
+	double x, y, z = 1;
+
+	if(!input.getPos(x, y)) {
+		servoX.write(0);
+		servoY.write(0);
+		return;
+	}
+
+	double zx = -x * MX / z;
+	double zy = -y * MY / z;
+
+	double angleX = zx / MX;
+	double angleY = zy / MY;
+
+	double DX = angleX * 1800 / M_PI;
+	double DY = angleY * 1800 / M_PI;
+
+	double USX = us2dc(DUTY_MS, CENTER_X_US + cap(DX));
+	double USY = us2dc(DUTY_MS, CENTER_Y_US + cap(DY));
+
+	printf("FXOS8700Q ACC: X=%1.4f Y=%1.4f Z=%1.4f DX=%f DY=%f\r\n", x, y, z, USX, USY);
+	servoX.write(USX);
+	servoY.write(USY);
 }
 
 
@@ -33,39 +69,8 @@ int main() {
 	panel.calibrateY(8600, 48900, true);
 	panel.setSwapXY(true);
 
+	EventQueue queue;
 
-#if INPUT_METHOD == INPUT_METHOD_TOUCH
-	TouchPanel &input = panel;
-#elif INPUT_METHOD == INPUT_METHOD_ACCELEROMETER
-	AccelerometerInput input;
-#else
-	#error "wrong input method"
-#endif
-
-	for(;;) {
-		double x, y, z = 1;
-
-		if(!input.getPos(x, y)) {
-			servoX.write(0);
-			servoY.write(0);
-			continue;
-		}
-
-		double zx = -x * MX / z;
-		double zy = -y * MY / z;
-
-		double angleX = zx / MX;
-		double angleY = zy / MY;
-
-		double DX = angleX * 1800 / M_PI;
-		double DY = angleY * 1800 / M_PI;
-
-		double USX = us2dc(DUTY_MS, CENTER_X_US + cap(DX));
-		double USY = us2dc(DUTY_MS, CENTER_Y_US + cap(DY));
-
-		printf("FXOS8700Q ACC: X=%1.4f Y=%1.4f Z=%1.4f DX=%f DY=%f\r\n", x, y, z, USX, USY);
-		servoX.write(USX);
-		servoY.write(USY);
-		wait(0.05f);
-	}
+	queue.call_every(MEASUREMENT_PERIOD_MS, control);
+	queue.dispatch();
 }
